bd17d122bb26cbee7743e5e575da93105fffadb6
Austin Narcomey <aon7@cs-aon7.cs.yale.internal>
2021-12-15 13:54:30 -0500

    adding performance analysis of our reward metrics

---
+++ RL/performance_analysis.py
@@ -0,0 +1,233 @@
+import matplotlib
+import numpy as np
+import sys
+import json
+from os import listdir
+from os.path import isfile, join
+from collections import defaultdict
+
+
+def load_episodes_from_logs(data_path):
+    discretized_mdp_files = [join(data_path, f) for f in listdir(data_path) if isfile(join(data_path, f)) and f.startswith('discretized')]
+    episodes = []
+
+    for discretized_mdp_file in discretized_mdp_files:
+        f = open(discretized_mdp_file)
+        data = json.load(f)
+        episode = []
+        photo_idx = -1
+
+        for i in range(len(data)):
+            timestep = data[i]
+            state = timestep[0]
+            action = timestep[1]
+            reward = timestep[2]
+
+            photo_idx = state["num_photos"]
+
+            s = tuple([state["subpolicy"], state["position"], int(state["starting_new_photo"] == True), state["user_position"][0], state["user_position"][1], state["user_position"][2]])
+            a = action
+            r = []
+
+            for raw_reward in reward:
+                scaled_reward = raw_reward - 4 if raw_reward != 0 else 0
+                r.append(scaled_reward)
+
+            r = tuple(r)
+
+            episode.append((s, a, r))
+
+            if i == len(data)-1 or data[i+1][0]["starting_new_photo"]:
+                episodes.append(episode)
+                episode = []
+
+    return episodes
+
+
+def compute_behavior_policy(subpolicy_to_num_positions, state, action):
+    if action == 100:
+        return 1./3
+    else:
+        return 2./3*1./subpolicy_to_num_positions[state[0]]
+
+
+def create_random_policy(nA):
+    """
+    Creates a random policy function.
+    
+    Args:
+        nA: Number of actions in the environment.
+    
+    Returns:
+        A function that takes an observation as input and returns a vector
+        of action probabilities
+    """
+    A = np.ones(nA, dtype=float) / nA
+    def policy_fn(observation):
+        return A
+    return policy_fn
+
+
+def create_greedy_policy(Q):
+    """
+    Creates a greedy policy based on Q values.
+    
+    Args:
+        Q: A dictionary that maps from state -> action values
+        
+    Returns:
+        A function that takes an observation as input and returns a vector
+        of action probabilities.
+    """
+    
+    def policy_fn(state):
+        A = np.zeros_like(Q[state], dtype=float)
+        best_action = np.argmax(Q[state])
+        A[best_action] = 1.0
+        return A
+    return policy_fn
+
+
+def mc_control_importance_sampling(episodes, discount_factor=1.0):
+    """
+    Monte Carlo Control Off-Policy Control using Weighted Importance Sampling.
+    Finds an optimal greedy policy.
+    
+    Args:
+        env: OpenAI gym environment.
+        num_episodes: Number of episodes to sample.
+        behavior_policy: The behavior to follow while generating episodes.
+            A function that given an observation returns a vector of probabilities for each action.
+        discount_factor: Gamma discount factor.
+    
+    Returns:
+        A tuple (Q, policy).
+        Q is a dictionary mapping state -> action values.
+        policy is a function that takes an observation as an argument and returns
+        action probabilities. This is the optimal greedy policy.
+    """
+
+    action_space_size = 101
+    num_rewards = 9
+    
+    # The final action-value function.
+    # A dictionary that maps state -> action values
+    
+
+    Qs = []
+    for i in range(num_rewards):
+        Q = defaultdict(lambda: np.zeros(action_space_size))
+        Qs.append(Q)
+
+    # The cumulative denominator of the weighted importance sampling formula
+    # (across all episodes)
+    Cs = []
+    for i in range(num_rewards):
+        C = defaultdict(lambda: np.zeros(action_space_size))
+        Cs.append(C)
+    
+    # Our greedily policy we want to learn
+
+    for i_reward in range(num_rewards):
+        print("Computing Q table for reward " + str(i_reward))
+        target_policy = create_greedy_policy(Qs[i_reward])
+            
+        for i_episode in range(1, len(episodes) + 1):
+            # Print out which episode we're on, useful for debugging.
+            print("Episode {}/{}.".format(i_episode, len(episodes)))
+
+            episode = episodes[i_episode-1]
+            
+            # Sum of discounted returns
+            G = 0.0
+            # The importance sampling ratio (the weights of the returns)
+            W = 1.0
+            # For each step in the episode, backwards
+            for t in range(len(episode))[::-1]:
+                state, action, reward = episode[t]
+                # Update the total reward since step t
+                G = discount_factor * G + reward[i_reward]
+                # Update weighted importance sampling formula denominator
+                Cs[i_reward][state][action] += W
+                # Update the action-value function using the incremental update formula (5.7)
+                # This also improves our target policy which holds a reference to Q
+                Qs[i_reward][state][action] += (W / Cs[i_reward][state][action]) * (G - Qs[i_reward][state][action])
+
+                target_policy = create_greedy_policy(Qs[i_reward])
+
+                # If the action taken by the behavior policy is not the action 
+                # taken by the target policy the probability will be 0 and we can break
+                if action !=  np.argmax(target_policy(state)):
+                    break
+                W = W * 1./compute_behavior_policy(subpolicy_to_num_positions, state, action)
+        
+    return Qs
+
+subpolicy_to_num_positions = {0: 21, 1: 21, 2: 24, 3: 27}
+
+#data_path = '/home/qz257/Projects/PhotoRL/shutter_tarzan/shutter-tarzan-shutter_rl/shutter_tarzan/rl_data_temp_updated/'
+#output_path = 'Q_table.json'
+
+#data_path = '/home/aon7/tarzan_ws/src/shutter-tarzan/shutter_tarzan/rl_data_all/'
+data_path = '/home/aon7/tarzan_ws/src/shutter-tarzan/shutter_tarzan/rl_data_test_uniform_policy/'
+
+
+episodes = load_episodes_from_logs(data_path)
+rewards = np.zeros((len(episodes), 9))
+rewards = []
+#print("episodes", episodes)
+#print(episodes[0])
+#print(len(episodes))
+for i, episode in enumerate(episodes):
+    #print("ep = ", episode)
+    curr_rewards = []#np.zeros((9))
+    for state, action, reward in episode:
+        if action == 100:
+            curr_rewards.append(reward)
+    rewards += curr_rewards
+    #print("rewards = ", rewards)
+    
+rewards = np.array(rewards)
+print("Rewards shape =", rewards.shape)
+print("Avg rewards = ", np.mean(rewards, axis=0))
+print("Std rewards = ", np.std(rewards, axis=0))
+
+
+
+
+''' 
+Rewards shape = (144,9)
+Result for rl_data_all:
+Avg rewards =  [-0.97222222 -0.84722222 -1.34027778 -1.34027778 -1.26388889  0.15972222 0.09027778  0.22222222 -0.13194444]
+Std rewards =  [3.06400347 1.88679418 1.77624446 1.65060256 1.73199512 0.67353952 0.58823175 0.75869446 0.58030796]
+
+Result for rl_data_test_uniform_policy
+Rewards shape = (9, 9)
+Avg rewards =  [0.88888889 0.55555556 0.77777778 0.77777778 0.77777778 0.55555556 0.55555556 0.55555556 0.33333333]
+Std rewards =  [1.19670329 1.34256066 1.68508343 1.3146844  1.74977953 0.83147942 0.83147942 0.83147942 0.66666667]
+'''
+
+
+
+
+
+
+
+
+
+
+#output_path = 'Q_table_all_data2.json'
+
+#episodes = load_episodes_from_logs(data_path)
+#Qs = mc_control_importance_sampling(episodes)
+
+#Qs_json = []
+
+#for i in range(len(Qs)):
+#    Q_json = {}
+#    for key, value in Qs[i].items():
+#        Q_json[str(key)] = list(value)
+#    Qs_json.append(Q_json)
+
+#with open(output_path, 'w') as outfile:
+#    json.dump(Qs_json, outfile)
